cc4a665e27239afd05098c4b736efbd176a3c024
masahiro-0420 <masahiro.kisogawa@icloud.com>
2021-03-21 00:29:42 +0900

    モデル単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname              {'test'}
     email                 {'test@example'}
-    encrypted_password    {'000aaa'}
-    password_confirmation {encrypted_password}
+    password              {'aaa000'}
+    password_confirmation {password}
     last_name             {'山田'}
     first_name            {'太郎'}
     last_name_kana        {'ヤマダ'}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,30 +19,30 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end 
-    it 'encrypted_passwordが空では登録できない' do
-      @user.encrypted_password = ''
+    it 'passwordが空では登録できない' do
+      @user.password = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Encrypted_password can't be blank")
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end   
     it 'last_nameが空では登録できない' do
       @user.last_name = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Last_name can't be blank")
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
     end   
     it 'first_nameが空では登録できない' do
       @user.first_name = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("First_name can't be blank")
+      expect(@user.errors.full_messages).to include("First name can't be blank")
     end   
     it 'last_name_kanaが空では登録できない' do
       @user.last_name_kana = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Last_name_kana can't be blank")
+      expect(@user.errors.full_messages).to include("Last name kana can't be blank")
     end   
     it 'first_name_kanaが空では登録できない' do
       @user.first_name_kana = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("First_name_kana can't be blank")
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
     end  
     it 'birthdayが空では登録できない' do
       @user.birthday = nil
@@ -53,36 +53,36 @@ RSpec.describe User, type: :model do
       @user.save
       another_user = FactoryBot.build(:user, email: @user.email)
       another_user.valid?
-      expect(another_user.errors.full_messages).to include('Email has already been taken')
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
     end
     it 'emailに＠が含まれていないと登録できない' do
       @user.email = 'testexample'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Email ")
+      expect(@user.errors.full_messages).to include("Email is invalid")
 
     end   
-    it 'encrypted_passwordが５文字以下では登録できない' do
-      @user.encrypted_password = '12345'
+    it 'passwordが５文字以下では登録できない' do
+      @user.password = '12345'
       @user.password_confirmation = '12345'
       @user.valid?
-      expect(@user.errors.full_messages).to include('Encrypted_password is too short (minimum is 6 characters)')
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
     it 'encrypted_passwordが半角英数字混合でないと登録できない' do
-      @user.encrypted_password = '000000'
+      @user.password = '000000'
       @user.password_confirmation = '000000'
       @user.valid?
-      expect(@user.errors.full_messages).to include('Encrypted_password is ')
+      expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
     end
     it 'encrypted_passwordが存在してもpassword_confirmationが空では登録できない' do
       @user.password_confirmation = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Encrypted_password")
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
     it 'encrypted_passwordとpassword_confirmationが一致しないと登録できない' do
-      @user.encrypted_password = '000aaa'
+      @user.password = '000aaa'
       @user.password_confirmation = 'aaa000'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Encrypted_password")
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end  
   end
 end
