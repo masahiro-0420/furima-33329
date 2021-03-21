1a928e7195b1d7b5c790d37e93a54bbce114b1ae
masahiro-0420 <masahiro.kisogawa@icloud.com>
2021-03-21 18:53:41 +0900

    itemモデルとアソシエーション作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_231338) do
+ActiveRecord::Schema.define(version: 2021_03_21_092704) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name"
+    t.text "explain"
+    t.integer "category_id"
+    t.integer "status_id"
+    t.integer "shipping_fee_id"
+    t.integer "prefecture_id"
+    t.integer "delivery_schedule_id"
+    t.integer "price"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_03_17_231338) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
