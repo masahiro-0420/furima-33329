848a0290db03db9462561b7575874ccadfe55565
masahiro-0420 <masahiro.kisogawa@icloud.com>
2021-03-21 19:57:56 +0900

    validates presence true の入力

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,17 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:item).permit(:item_name, :explain, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_schedule_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+
+    validates :item_name
+    validates :explain
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :delivery_schedule_id 
+    validates :price
+    validates :image
+
+  end
 end
