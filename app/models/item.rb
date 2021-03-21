be6e8c56ec2ae53b272fde9aeecdce8d71522d72
masahiro-0420 <masahiro.kisogawa@icloud.com>
2021-03-22 08:18:24 +0900

    ActiveHash::Baseのモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :delivery_schedule
+
   belongs_to :user
   has_one_attached :image
 
@@ -6,16 +13,21 @@ class Item < ApplicationRecord
 
     validates :item_name
     validates :explain
-    validates :category_id
-    validates :status_id
-    validates :shipping_fee_id
-    validates :prefecture_id
-    validates :delivery_schedule_id 
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_fee_id 
+      validates :prefecture_id
+      validates :delivery_schedule_id
+    end
+
     validates :price, :numericality => {
       :greater_than_or_equal_to => 300,
       :less_than_or_equal_to => 9999999,
       :message => '入力値が300~9,999,999の範囲外です' 
     }, format: {with: /\A[0-9]+\z/, message: 'は半角数字以外では登録できません'}
+    
     validates :image
 
   end
