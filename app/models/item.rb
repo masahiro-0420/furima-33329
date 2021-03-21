b2e7c89e893f5effc4f2c29d2a9cec5eaed6d930
masahiro-0420 <masahiro.kisogawa@icloud.com>
2021-03-21 21:08:59 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
 
@@ -10,7 +11,11 @@ class Item < ApplicationRecord
     validates :shipping_fee_id
     validates :prefecture_id
     validates :delivery_schedule_id 
-    validates :price
+    validates :price, :numericality => {
+      :greater_than_or_equal_to => 300,
+      :less_than_or_equal_to => 9999999,
+      :message => '入力値が300~9,999,999の範囲外です' 
+    }, format: {with: /\A[0-9]+\z/, message: 'は半角数字以外では登録できません'}
     validates :image
 
   end
