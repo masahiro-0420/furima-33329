2db66581261d50fd98223f6899793ab72fffb8ab
masahiro-0420 <masahiro.kisogawa@icloud.com>
2021-03-22 22:10:04 +0900

    test_image.pngの追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
     
+
+
+    
+    association :user
+
+    after(:build) do |item|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の情報を入力' do
+
+    it 'nameの値が存在すれば登録できること' do
+
+    end
+
+    it 'nameが空では登録できないこと' do
+
+    end
+
+
+
+  end
 end
