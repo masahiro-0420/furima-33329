b73d3b38615a6d73d5cf5707ed70ea84396a6498
masahiro-0420 <masahiro.kisogawa@icloud.com>
2021-03-28 17:11:00 +0900

    null: falseの追加

--- db/migrate/20210321092704_create_items.rb
+++ db/migrate/20210321092704_create_items.rb
@@ -1,16 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string  :item_name
-      t.text  :explain
-      t.integer :category_id
-      t.integer :status_id 
-      t.integer :shipping_fee_id 
-      t.integer :prefecture_id
-      t.integer :delivery_schedule_id
-      t.integer :price
+      t.string  :item_name,            null: false
+      t.text  :explain,                null: false  
+      t.integer :category_id,          null: false  
+      t.integer :status_id ,           null: false  
+      t.integer :shipping_fee_id,      null: false  
+      t.integer :prefecture_id,        null: false  
+      t.integer :delivery_schedule_id, null: false  
+      t.integer :price,                null: false  
       t.references :user, foreign_key: true
-      t.timestamps
+      t.timestamps,                    null: false  
     end
   end
 end
