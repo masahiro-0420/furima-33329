57a08f228fbf729e435d7db9808ab2840e69b470
masahiro-0420 <masahiro.kisogawa@icloud.com>
2021-03-28 11:39:16 +0900

    数字のクオーテーション削除、テストコード追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     item_name               {'test'}
     explain                 {'test'}
-    category_id             {'2'}
-    status_id               {'2'}
-    shipping_fee_id         {'2'}
-    prefecture_id           {'2'}
-    delivery_schedule_id    {'2'}
-    price                   {'1000'}
+    category_id             {2}
+    status_id               {2}
+    shipping_fee_id         {2}
+    prefecture_id           {2}
+    delivery_schedule_id    {2}
+    price                   {1000}
     
     association :user
 
