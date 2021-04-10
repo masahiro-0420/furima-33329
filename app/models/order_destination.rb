53f58dad444ca757869a743e11d87a4d65bc990e
masahiro-0420 <masahiro.kisogawa@icloud.com>
2021-04-11 08:45:32 +0900

    validationを記載

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,4 +1,13 @@
 class OrderDestination
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "はハイフンが必要です" }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{1,11}\z/, message: "は11桁以内でハイフンが不要です." }   
+  end
+
 end
