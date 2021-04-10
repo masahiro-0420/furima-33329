87c672fd373e112f63076ea88eb5249bb5efcbff
masahiro-0420 <masahiro.kisogawa@icloud.com>
2021-04-10 11:23:06 +0900

    order controllerを実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,13 +2,21 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-
+    @order = Order.new
   end
 
   def create
-
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
-
+  def order_params
+    params.require(:order).permit(:price)
+  end
 
 end
